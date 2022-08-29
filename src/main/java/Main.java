4b34d7b9ece062aabeef46a32a994efc09b401de
Andrea200421 <andreaagqu@unisabana.edu.co>
2022-08-29 08:23:03 -0500

    Substraction class created
    I created substraction class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Substraction substraction = new Substraction();
+        System.out.println(substraction.substraction(6,3));
     }
 }
---
+++ src/main/java/Substraction.java
@@ -0,0 +1,5 @@
+public class Substraction {
+    public int substraction(int x, int y){
+        return x-y;
+    }
+}
