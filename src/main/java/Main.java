31b3cd8e495f5d7af2778ac00cca4154296f9f98
David Alejandro Espitia Cortés <davidesco2001@gmail.com>
2022-08-29 08:12:12 -0500

    Multiply class created and implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Multiply multiply = new Multiply();
+        System.out.println(multiply.multiplyNumbers(4, 5));
     }
 }
